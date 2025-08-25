c33c6fd09c3a43e07030944e612b3a88a7aca0fe
dev-crusader404 <57078175+dev-crusader@users.noreply.github.com>
2025-08-25 19:05:03 +1000

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import Todos from "./components/Todos";
 import TodoItem from "./components/TodoItem";
 import CreateTodo from "./components/CreateTodo";
@@ -9,6 +10,26 @@ import Signup from "./components/Signup";
 const App = () => {
   return (
     <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Todo App
+          </Typography>
+          <Button color="inherit" component={Link} to="/todos">
+            Todos
+          </Button>
+          <Button color="inherit" component={Link} to="/todos/new">
+            Create Todo
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Signup
+          </Button>
+        </Toolbar>
+      </AppBar>
+
       <Routes>
         <Route exact path="/" element={<Todos />} />
         <Route exact path="/todos" element={<Todos />} />
