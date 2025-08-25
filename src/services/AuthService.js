57ac65cdf432b608cecf73419f8dce2de0e90cbe
dev-crusader404 <57078175+dev-crusader@users.noreply.github.com>
2025-08-25 19:05:24 +1000

    authentication function added

---
+++ src/services/AuthService.js
@@ -0,0 +1,21 @@
+export const login = async (username, password) => {
+  const response = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+};
+
+export const signup = async (username, password) => {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+};
